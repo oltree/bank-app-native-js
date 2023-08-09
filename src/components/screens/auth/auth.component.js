4ec552c5887ca50f905d83136ec10b122637a9a0
oltree <o.melex@bk.ru>
2023-08-09 20:48:34 +0300

    feat: added auth form

--- src/components/screens/auth/auth.component.js
+++ src/components/screens/auth/auth.component.js
@@ -1,22 +1,71 @@
-import { ScreenComponent } from '@/core/component';
-import { RenderService } from '@/core/services';
+import { AuthService } from '@/api/auth.service';
 
-import { Heading } from '@/components/ui/heading';
+import { Button } from '@/components/ui/button';
+import { Field } from '@/components/ui/field';
+
+import { $R } from '@/core/rquery';
+import { RenderService } from '@/core/services';
+import { ScreenComponent } from '@/core/component';
 
 import styles from './auth.module.scss';
 import template from './auth.template.html';
 
 export class Auth extends ScreenComponent {
+  #isTypeLogin = true;
+
   constructor() {
     super({ title: 'Auth' });
+
+    this.authService = new AuthService();
   }
+
+  #handleSubmit = (event) => {
+    console.log(event.target);
+  };
+
+  #changeFormType = (event) => {
+    event.preventDefault();
+
+    $R(this.element)
+      .find('h1')
+      .text(this.#isTypeLogin ? 'Register' : 'Sign In');
+
+    $R(event.target).text(this.#isTypeLogin ? 'Sign In' : 'Register');
+
+    this.#isTypeLogin = !this.#isTypeLogin;
+  };
 
   render() {
     this.element = RenderService.htmlToElement(
       template,
-      [new Heading('Auth')],
+      [
+        new Button({
+          children: 'Submit',
+        }),
+      ],
       styles
     );
+
+    $R(this.element)
+      .find('#auth-inputs')
+      .append(
+        new Field({
+          placeholder: 'Enter email',
+          name: 'email',
+          type: 'email',
+        }).render()
+      )
+      .append(
+        new Field({
+          placeholder: 'Enter password',
+          name: 'password',
+          type: 'password',
+        }).render()
+      );
+
+    $R(this.element).find('#change-form-type').click(this.#changeFormType);
+
+    $R(this.element).find('form').submit(this.#handleSubmit);
 
     return this.element;
   }
